fb8530a69e913370715871dc86dd75eb3a8ac544
ZivojinMisic <97547019+ZivojinMisic@users.noreply.github.com>
2022-01-31 10:17:01 +0100

    Add files via upload

---
+++ cas02/practice.js
@@ -0,0 +1,17 @@
+// -------------------------------   VEZBANJE   ---------------------------------------------------------
+// Nova terminologija: snippet code - isecak koda, u ovom slucaju ce biti deo skripte zbog kratkih resenja.
+// Napisati javascript snippet code koji :
+// 1. Ispisuje prosledjeni niz brojeva jedan ispod drugog.
+// 2. Od prosledjenjog niza brojeva ispisuje samo parne 
+// 3. Racuna sumu prosledjenog niza
+// 4. Ispisuje koliko ima brojeva vecih od prvog elementa niza
+a=[]
+for(i=0;i<a.length;i++)
+    console.log(i);
+
+for(i=0;i<a.length;i++)
+    if(i%2==0)
+     console.log(i);
+
+//izvinite, nisam stigao da zavrsim
+    
